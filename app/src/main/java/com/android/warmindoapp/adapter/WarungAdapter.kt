e4aac89b04e6957d0c90a35a0e3147405f836920
Rizukiuf <rizkiutamafauzi@gmail.com>
2023-12-21 10:43:46 +0700

    add cloud storage firebase, gambar for warung

--- app/src/main/java/com/android/warmindoapp/adapter/WarungAdapter.kt
+++ app/src/main/java/com/android/warmindoapp/adapter/WarungAdapter.kt
@@ -3,20 +3,25 @@ package com.android.warmindoapp.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.android.warmindoapp.R
 import com.android.warmindoapp.data.entity.Warung
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
 
 class WarungAdapter(private var list:List<Warung>) : RecyclerView.Adapter<WarungAdapter.ViewHolder>() {
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var namawarung : TextView
         var idwarung : TextView
+        var gambarwarung : ImageView
 
         init {
             namawarung = view.findViewById(R.id.tv_namawarung)
             idwarung = view.findViewById(R.id.tv_idwarung)
+            gambarwarung = view.findViewById(R.id.iv_gambarwarung)
         }
     }
 
@@ -32,6 +37,23 @@ class WarungAdapter(private var list:List<Warung>) : RecyclerView.Adapter<Warung
     override fun onBindViewHolder(holder: WarungAdapter.ViewHolder, position: Int) {
         holder.namawarung.text = list[position].namawarung
         holder.idwarung.text = list[position].idwarung.toString()
+
+        // Mengambil URL gambar dari Firebase Storage
+        val storageReference = FirebaseStorage.getInstance().reference
+        val pathReference =
+            list[position].gambar?.let { storageReference.child(it) } // Ambil path dari kolom gambar
+
+        if (pathReference != null) {
+            pathReference.downloadUrl.addOnSuccessListener { uri ->
+                // Uri adalah URL gambar dari Firebase Storage
+                // Gunakan library gambar seperti Glide atau Picasso untuk menampilkan gambar di ImageView
+                Glide.with(holder.itemView.context)
+                    .load(uri)
+                    .into(holder.gambarwarung)
+            }.addOnFailureListener { exception ->
+                // Handle jika terjadi kesalahan dalam mengambil URL gambar
+            }
+        }
     }
 
 }
